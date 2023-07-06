cfe059368789a6a0bc322f7ec7912d58b27f72b1
tujianglin <jianglin.tu@wimisoft.com>
2023-07-06 13:31:25 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import electron from 'vite-plugin-electron'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    electron({
+      entry: 'electron/main.ts',
+      vite: {
+        build: {
+          rollupOptions: {
+            external: ['serialport', 'sqlite3'],
+          },
+        },
+      },
+    }),
+  ],
+})
